9167d879bdca6a537ed4998af4902597c6a52b1d
Noah Anderson <noahbanderson@gmail.com>
2022-02-12 09:51:22 -0700

    fix(admin): only allow admins access to the admin app

--- src/admin/admin.factory.ts
+++ src/admin/admin.factory.ts
@@ -8,12 +8,15 @@ import { ConfigModule, ConfigService } from 'config'
 export const adminFactory: AdminModuleFactory = {
 	imports: [AuthenticationModule, ConfigModule],
 	inject: [AuthenticationService, ConfigService],
-	useFactory: (
-		{ validateLocal: authenticate }: AuthenticationService,
-		configService: ConfigService,
-	) => ({
+	useFactory: (authService: AuthenticationService, configService: ConfigService) => ({
 		auth: {
-			authenticate,
+			authenticate: async (email, password) => {
+				const user = await authService.validateLocal(email, password)
+
+				if (user?.isAdmin ?? false) return user
+
+				return null
+			},
 			cookieName: 'nest-admin',
 			cookiePassword: configService.get('ADMIN_COOKIE_SECRET'),
 		},
